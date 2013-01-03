03034081e0488d3b564caeb1d994d15dc1d2528d
Larry Witham <witham.larry.dev@gmail.com>
2013-01-02 23:45:12 -0500

    Added Express

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
-var app = require('http').createServer(handler)
-, io = require('socket.io').listen(app)
-, fs = require('fs')
+var express=require("express");
+var app = express();
+var io = require('socket.io').listen(app);
 
-app.listen(process.env.PORT || 8001);
+var port=process.env.PORT||8001;
+app.listen(port);
 
-function handler (req, res) {
-fs.readFile('index.html',
-function (err, data) {
-if (err) {
-res.writeHead(500);
-return res.end('Error loading index.html');
-}
-
-res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-res.end(data);
+app.get('/', function (req, res) {
+res.sendfile(__dirname + '/index.html');
 });
-}
 
 io.sockets.on('connection', function (socket) {
 // echo the message
